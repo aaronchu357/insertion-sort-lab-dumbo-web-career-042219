44963e56ca77e078ccf2d158784a3d318c5b8dc5
Aaron Chu <aaronchu357@gmail.com>
2019-10-16 18:38:14 -0400

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 function findMinAndRemove(array){
-
+  let min = array[0]
+  let i = 1
+  while(i < array.length){
+    if(min > array[i]){
+      min = array[i]
+    }
+    i++;
+  }
+  let position = array.indexOf(min)
+  // return array.slice(0, position).concat(array.slice(position + 1))
+  return array.splice(position, 1)[0]
 }
 
 function insertionSort(array){
-
+  let sortedArr = []
+  while(array.length !== 0){
+    let min = findMinAndRemove(array)
+    sortedArr.push(min)
+  }
+  return sortedArr;
 }
